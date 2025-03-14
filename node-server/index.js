b8a13c65b3c71bdca96f9ae355f3f60fbdfa3298
Kachkynov Alikhan <t8t228@gmail.com>
2025-03-14 13:57:52 +0600

    Exercise 1 has finished

--- node-server/index.js
+++ node-server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/hello', (req, res) => {
+   return res.send('Hello');
+});
+
 app.listen(port, () => {
    console.log(`Server started at http://localhost:${port}`);
 });
