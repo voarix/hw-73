096c234c2f98eb7ee8cde9ddfde001d603a62ba0
Kachkynov Alikhan <t8t228@gmail.com>
2025-03-14 14:38:23 +0600

    Exercise 2 has finished

--- node-server/index.js
+++ node-server/index.js
@@ -3,10 +3,20 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const Vigenere = require('caesar-salad').Vigenere;
+
 app.get('/hello', (req, res) => {
    return res.send('Hello');
 });
 
+app.get('/encode/:value', (req, res) => {
+   return res.send(Vigenere.Cipher('password').crypt(req.params.value));
+});
+
+app.get('/decode/:value', (req, res) => {
+   return res.send(Vigenere.Decipher('password').crypt(req.params.value));
+});
+
 app.listen(port, () => {
-   console.log(`Server started at http://localhost:${port}`);
+   console.log(`Server listening at http://localhost:${port}`);
 });
